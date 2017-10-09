3229d420282fadfe975b12e653d9b23ce67e417b
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-10-09 19:44:57 +0200

    Ejercicio finalizado

---
+++ src/boletin2_02/Boletin2_02.java
@@ -0,0 +1,11 @@
+package boletin2_02;
+
+public class Boletin2_02 {
+
+    public static void main(String[] args) {
+        float lado = 3f, area;
+        area= lado*lado;
+        System.out.println("Area cuadrado = "+area);
+    }
+    
+}
